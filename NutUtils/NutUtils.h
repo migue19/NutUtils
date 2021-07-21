2dbbe97420c7944a4f5a033bd261073cc5334c57
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2021-07-21 10:53:48 -0500

    Initial Commit

---
+++ NutUtils/NutUtils.h
@@ -0,0 +1,18 @@
+//
+//  NutUtils.h
+//  NutUtils
+//
+//  Created by Miguel Mexicano Herrera on 21/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NutUtils.
+FOUNDATION_EXPORT double NutUtilsVersionNumber;
+
+//! Project version string for NutUtils.
+FOUNDATION_EXPORT const unsigned char NutUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NutUtils/PublicHeader.h>
+
+
